cc73909ec49cffa7c465c4a6467a659bd67d3c3a
alexfeng262 <alexfeng262@gmail.com>
2019-01-08 13:11:13 +0100

    Cambio a promesas

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,12 +1,48 @@
 //Inicializa el modelo de datos
 const Product = require('../models/product.model');
 
+//Lista todos los productos PROMISES
+function product_listp(req){
+    return new Promise(function(resolve,reject){
+        Product.find({}).exec(function(err, products){
+            if( err ){ console.log('Error: ', err); reject(); }
+            console.log("The INDEX");
+            resolve(products);
+        });
+    });
+}
+
+//Modulo que guarda el producto en MongoDB PROMISES
+function savep(req){
+    return new Promise(function(resolve,reject){
+        var product = new Product({
+            name: req.body.name,
+            price: req.body.price,
+            qty: req.body.qty 
+        });
+        
+        product.save(function(err){
+            if( err ){ console.log('Error: ', err); reject(); }
+            
+            console.log("Successfully created a product. :)");
+            resolve();  
+        });
+    })
+}
+
+//Modulo que muestra el detalle de un producto en especifico PROMISES
+function showp(req){
+    return new Promise(function(resolve,reject){
+        Product.findOne({_id: req.params.id}).exec(function(err, product){
+            if( err ){ console.log('Error: ', err); reject(); }
+            resolve(product);
+        });
+    })
+}
+
 //Lista todos los productos
 exports.product_list = function(req, res){
-    
-    Product.find({}).exec(function(err, products){
-        if( err ){ console.log('Error: ', err); return; }
-        console.log("The INDEX");
+    product_listp(req).then(function(products){
         res.render('../views/index', {products: products} );  
     });
 };
@@ -19,27 +55,14 @@ exports.create = function(req, res){
  
 //Modulo que guarda el producto en MongoDB
 exports.save = function(req, res){
-
-    var product = new Product({
-        name: req.body.name,
-        price: req.body.price,
-        qty: req.body.qty 
-    });
-    
-    product.save(function(err){
-        if( err ){ console.log('Error: ', err); return; }
-        
-        console.log("Successfully created a product. :)");
+    savep(req).then(function(){
         res.redirect("/products");
-        
-    });
+    })           
 };
+
 //Modulo que muestra el detalle de un producto en especifico 
 exports.show = function(req, res){
-    Product.findOne({_id: req.params.id}).exec(function(err, product){
-        if( err ){ console.log('Error: ', err); return; }
-        
+    showp(req).then(function(product){
         res.render('../views/product/show', {product: product} );
     });
-    
 };
